916317efb6a83f9f4f0c9d9c04d64d63abe44658
王林 <wanglin07@bilibili.com>
2022-06-13 16:16:06 +0800

    feat: add sentry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as Sentry from "@sentry/vue"
+import { BrowserTracing } from "@sentry/tracing"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: "https://a067ce78f46d45de8ef67ba45385cad3@o1286624.ingest.sentry.io/6500629",
+  integrations: [
+    new BrowserTracing({
+      // routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
+app.mount('#app')
